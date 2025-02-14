11007326e703624fb2b9cc68a530f514ff40e297
Sarah Dutkiewicz <sarah.dutkiewicz@nimblepros.com>
2025-02-14 10:15:58 -0500

    Moving the shared auth to BlazorShared, out of BlazorAdmin

--- src/BlazorShared/Authorization/UserInfo.cs
+++ src/BlazorShared/Authorization/UserInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace BlazorAdmin.Authorization;
+namespace BlazorShared.Authorization;
 
 public class UserInfo
 {
--- src/Web/Controllers/UserController.cs
+++ src/Web/Controllers/UserController.cs
@@ -1,5 +1,5 @@
 ﻿using System.Security.Claims;
-using BlazorAdmin.Authorization;
+using BlazorShared.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
